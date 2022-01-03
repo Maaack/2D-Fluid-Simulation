22992cefa2b7b25cd9cdc75366b789e69870b8f4
Maaack <marek.belski@gmail.com>
2022-01-02 21:21:46 -0800

    INIT-01: Use Godot shader built-ins for texture and pixel size.

--- Main.gd
+++ Main.gd
@@ -1,9 +1,1 @@
 extends Node2D
-
-func _ready():
-	var dye_texture = $DyeViewport.get_texture()
-	var velocity_texture = $VelocityViewport.get_texture()
-	$DyeViewport/Icon.material.set_shader_param("velocity", velocity_texture)
-	$DyeViewport/Icon.material.set_shader_param("advected", dye_texture)
-	$VelocityViewport/Icon.material.set_shader_param("velocity", velocity_texture)
-	$VelocityViewport/Icon.material.set_shader_param("advected", velocity_texture)
