43e0485601661829e81c572abf4a9057a593109d
Maaack <marek.belski@gmail.com>
2022-02-18 20:30:19 -0800

    INIT-02: Start new multipass scene. Improvements to single pass.

---
+++ Scenes/MultiPass.gd
@@ -0,0 +1,103 @@
+extends Node
+
+
+enum Options{
+	DYE,
+	DIVERGENCE,
+	PRESSURE,
+	PRESSURE_FORCE,
+	VISCOSITY,
+	}
+
+var velocity_source_next : bool = false
+var scale_brush_force : float = 0.1;
+
+func _refresh_velocity():
+	$VelocityViewport/Sprite.hide()
+	yield(get_tree().create_timer(0.25), "timeout")
+	$VelocityViewport/Sprite.show()
+
+func _refresh_icon():
+	$DyeViewport/Icon.show()
+	yield(get_tree().create_timer(0.25), "timeout")
+	$DyeViewport/Icon.hide()
+
+func _refresh_clear():
+	$DyeViewport/Sprite.hide()
+	yield(get_tree().create_timer(0.25), "timeout")
+	$DyeViewport/Sprite.show()
+
+func _switch_velocity():
+	velocity_source_next = not velocity_source_next
+	if velocity_source_next:
+		$VelocityViewport/Sprite.texture = $GradientSubtractionViewport.get_texture()
+	else:
+		$VelocityViewport/Sprite.texture = $VelocityViewport.get_texture()
+	
+
+func _ready():
+	var velocity_texture = $ViscosityViewport.get_texture()
+	$VelocityViewport/Sprite.texture = velocity_texture
+	$VelocityViewport/Sprite.material.set_shader_param("velocity", $GradientSubtractionViewport.get_texture())
+	var dye_texture = $BackBufferViewport.get_texture()
+	$DyeViewport/Sprite.texture = dye_texture
+	_refresh_velocity()
+	_refresh_icon()
+
+func _process(delta):
+	$DyeViewport/Sprite.material.set_shader_param("deltaTime", delta)
+	$VelocityViewport/Sprite.material.set_shader_param("deltaTime", delta)
+	$ViscosityViewport.set_shader_param("deltaTime", delta)
+
+func _on_RefreshVelocityButton_pressed():
+	_refresh_velocity()
+
+func _on_SwitchVelocityButton_pressed():
+	_switch_velocity()
+
+func _on_RefreshIconButton_pressed():
+	_refresh_icon()
+
+func _on_RefreshClearButton_pressed():
+	_refresh_clear()
+
+func _on_PressureLevelSpinBox_value_changed(value):
+	$PressureViewport.levels = value
+
+func _on_ViscosityLevelSpinBox_value_changed(value):
+	$ViscosityViewport.levels = value
+
+func _on_ViscosityMeasureSpinBox_value_changed(value):
+	$ViscosityViewport.set_shader_param("viscosity", value)
+
+func _on_VorticityMeasureSpinBox_value_changed(value):
+	$VelocityForcesViewport/Sprite.material.set_shader_param("vorticity", value)
+
+func _on_BordersOptionButton_toggled(button_pressed):
+	$VelocityBorderViewport/Sprite.material.set_shader_param("active", button_pressed)
+
+func _on_MouseControl_force_applied(position, vector):
+	vector *= scale_brush_force
+	$VelocityViewport/Sprite.material.set_shader_param("brushCenterUV", position)
+	$VelocityViewport/Sprite.material.set_shader_param("brushColor", Color(vector.x, vector.y, 0.0, 1.0))
+	$VelocityViewport/Sprite.material.set_shader_param("brushOn", true)
+	$DyeViewport/Sprite.material.set_shader_param("brushCenterUV", position)
+	$DyeViewport/Sprite.material.set_shader_param("brushOn", true)
+
+func _on_MouseControl_force_released(position):
+	$VelocityViewport/Sprite.material.set_shader_param("brushColor", Color.black)
+	$VelocityViewport/Sprite.material.set_shader_param("brushOn", false)
+	$DyeViewport/Sprite.material.set_shader_param("brushOn", false)
+
+func _on_FinalViewButton_item_selected(index):
+	match(index):
+		Options.DYE:
+			$FinalViewportSprite.texture = $DyeViewport.get_texture()
+		Options.DIVERGENCE:
+			$FinalViewportSprite.texture = $DivergenceViewport.get_texture()
+		Options.PRESSURE:
+			$FinalViewportSprite.texture = $PressureBorderViewport.get_texture()
+		Options.PRESSURE_FORCE:
+			$FinalViewportSprite.texture = $GradientSubtractionViewport.get_texture()
+		Options.VISCOSITY:
+			$FinalViewportSprite.texture = $VelocityBorderViewport.get_texture()
--- Scenes/SinglePass.gd
+++ Scenes/SinglePass.gd
@@ -11,22 +11,17 @@ enum Options{
 	}
 
 var velocity_source_next : bool = false
-
+var scale_brush_force : float = 0.5;
 
 func _refresh_velocity():
-	$VelocityViewport/Icon.hide()
-	yield(get_tree().create_timer(0.25), "timeout")
 	$VelocityViewport/Icon2.show()
 	yield(get_tree().create_timer(0.25), "timeout")
 	$VelocityViewport/Icon2.hide()
-	$VelocityViewport/Icon.show()
 
 func _refresh_icon():
-	$DyeViewport/Sprite.hide()
 	$DyeViewport/Icon.show()
 	yield(get_tree().create_timer(0.25), "timeout")
 	$DyeViewport/Icon.hide()
-	$DyeViewport/Sprite.show()
 
 func _refresh_clear():
 	$DyeViewport/Sprite.hide()
@@ -34,16 +29,16 @@ func _refresh_clear():
 	$DyeViewport/Sprite.show()
 
 func _ready():
-	_refresh_velocity()
-	_refresh_icon()
 	var velocity_texture = $VelocityBackBufferViewport.get_texture()
-	$VelocityViewport/Icon.texture = velocity_texture
+	$VelocityViewport/Sprite.texture = velocity_texture
 	var dye_texture = $BackBufferViewport.get_texture()
 	$DyeViewport/Sprite.texture = dye_texture
+	_refresh_velocity()
+	_refresh_icon()
 
 func _process(delta):
 	$DyeViewport/Sprite.material.set_shader_param("deltaTime", delta)
-	$VelocityViewport/Icon.material.set_shader_param("deltaTime", delta)
+	$VelocityViewport/Sprite.material.set_shader_param("deltaTime", delta)
 
 func _on_RefreshVelocityButton_pressed():
 	_refresh_velocity()
@@ -55,23 +50,25 @@ func _on_RefreshClearButton_pressed():
 	_refresh_clear()
 
 func _on_PressureScaleSpinBox_value_changed(value):
-	$VelocityViewport/Icon.material.set_shader_param("pressureScale", value)
+	$VelocityViewport/Sprite.material.set_shader_param("pressureScale", value)
 	$PressureViewport/Sprite.material.set_shader_param("pressureScale", value)
 	$PressureForceViewport/Sprite.material.set_shader_param("pressureScale", value)
 
 func _on_PressureMinSpinBox_value_changed(value):
-	$VelocityViewport/Icon.material.set_shader_param("pressureMin", value)
+	$VelocityViewport/Sprite.material.set_shader_param("pressureMin", value)
 
 func _on_PressureMaxSpinBox_value_changed(value):
-	$VelocityViewport/Icon.material.set_shader_param("pressureMax", value)
+	$VelocityViewport/Sprite.material.set_shader_param("pressureMax", value)
 
 func _on_MouseVelocityControl_force_applied(position, vector):
-	$VelocityViewport/Icon.material.set_shader_param("externalForceVector", vector)
-	$VelocityViewport/Icon.material.set_shader_param("externalForceUV", position)
-	$VelocityViewport/Icon.material.set_shader_param("externalForceOn", true)
+	vector *= scale_brush_force
+	$VelocityViewport/Sprite.material.set_shader_param("externalForceVector", vector)
+	$VelocityViewport/Sprite.material.set_shader_param("externalForceUV", position)
+	$VelocityViewport/Sprite.material.set_shader_param("externalForceOn", true)
 
 func _on_MouseVelocityControl_force_released(position):
-	$VelocityViewport/Icon.material.set_shader_param("externalForceOn", false)
+	$VelocityViewport/Sprite.material.set_shader_param("externalForceUV", Color.black)
+	$VelocityViewport/Sprite.material.set_shader_param("externalForceOn", false)
 
 func _on_MouseDyeControl_force_applied(position, vector):
 	$DyeViewport/Sprite.material.set_shader_param("brushCenterUV", position)
@@ -81,29 +78,29 @@ func _on_MouseDyeControl_force_released(position):
 	$DyeViewport/Sprite.material.set_shader_param("brushOn", false)
 
 func _on_LaplacianSpinBox_value_changed(value):
-	$VelocityViewport/Icon.material.set_shader_param("laplacianScale", value)
+	$VelocityViewport/Sprite.material.set_shader_param("laplacianScale", value)
 	$LaplacianViewport/Sprite.material.set_shader_param("laplacianScale", value)
 
 func _on_VorticitySpinBox_value_changed(value):
-	$VelocityViewport/Icon.material.set_shader_param("vorticityScale", value)
+	$VelocityViewport/Sprite.material.set_shader_param("vorticityScale", value)
 	$VorticityViewport/Sprite.material.set_shader_param("vorticityScale", value)
 
 func _on_BordersOptionButton_toggled(button_pressed):
-	$VelocityViewport/Icon.material.set_shader_param("borderOn", button_pressed)
+	$VelocityViewport/Sprite.material.set_shader_param("borderOn", button_pressed)
 
 func _on_FinalViewButton_item_selected(index):
 	match(index):
 		Options.DYE:
-			$FinalViewportIcon.texture = $DyeViewport.get_texture()
+			$FinalViewportSprite.texture = $DyeViewport.get_texture()
 		Options.DIVERGENCE:
-			$FinalViewportIcon.texture = $DivergenceViewport.get_texture()
+			$FinalViewportSprite.texture = $DivergenceViewport.get_texture()
 		Options.PRESSURE:
-			$FinalViewportIcon.texture = $PressureViewport.get_texture()
+			$FinalViewportSprite.texture = $PressureViewport.get_texture()
 		Options.PRESSURE_FORCE:
-			$FinalViewportIcon.texture = $PressureForceViewport.get_texture()
+			$FinalViewportSprite.texture = $PressureForceViewport.get_texture()
 		Options.LAPLACIAN:
-			$FinalViewportIcon.texture = $LaplacianViewport.get_texture()
+			$FinalViewportSprite.texture = $LaplacianViewport.get_texture()
 		Options.CURL:
-			$FinalViewportIcon.texture = $CurlViewport.get_texture()
+			$FinalViewportSprite.texture = $CurlViewport.get_texture()
 		Options.VORTICITY:
-			$FinalViewportIcon.texture = $VorticityViewport.get_texture()
+			$FinalViewportSprite.texture = $VorticityViewport.get_texture()
